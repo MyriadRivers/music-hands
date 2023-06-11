d0f0e6c95f01a703e4928a52763831ac1b7730ab
Jason Gao <jgaominneapolis@gmail.com>
2023-06-10 23:07:08 -0400

    Test look without connectors

--- src/App.tsx
+++ src/App.tsx
@@ -480,6 +480,7 @@ function App() {
 
         let canvasCtx = canvas.current.getContext("2d");
 
+        // Draw connectors and landmarks
         if (canvasCtx) {
           canvasCtx.save();
           canvasCtx.clearRect(0, 0, canvas.current.width, canvas.current.height);
